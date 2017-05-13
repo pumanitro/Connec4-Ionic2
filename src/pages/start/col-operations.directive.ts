1f623dbde9ed19b8b5099bd26594798930b3d8dc
pumanitro <pumanitro@gmail.com>
2017-05-13 14:18:48 +0200

    Created directive for column managing.

---
+++ src/pages/start/col-operations.directive.ts
@@ -0,0 +1,20 @@
+import {Directive, Input} from '@angular/core';
+
+/**
+ * Generated class for the ColOperations directive.
+ *
+ * See https://angular.io/docs/ts/latest/api/core/index/DirectiveMetadata-class.html
+ * for more info on Angular Directives.
+ */
+@Directive({
+  selector: '[col-operations]' // Attribute selector
+})
+export class ColOperations {
+
+  @Input() rowNumber: number;
+  @Input() colNumber: number;
+
+  constructor() {
+  }
+
+}
--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -1,5 +1,6 @@
 import {Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {ColOperations} from './col-operations.directive';
 
 /**
  * Generated class for the Start page.
