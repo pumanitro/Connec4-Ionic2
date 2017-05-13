abc52b03cd813d1eff07c641f44c711bac2df65e
pumanitro <pumanitro@gmail.com>
2017-05-13 11:19:22 +0200

    Circle have possibility to dont resize when we give percentage size.

--- src/pages/start/circle/circle.ts
+++ src/pages/start/circle/circle.ts
@@ -1,15 +1,36 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'circle',
   templateUrl: './circle.html'
 })
 export class Circle {
-  @Input() size:string;
+  @Input() size:string|number;
   @Input() color:string;
-  @Input() centered:boolean = true;
+  @Input() isCentered:boolean = true;
+  @Input() isEqualSize:boolean = true;
+
+  @ViewChild('circle') circle: ElementRef;
 
   constructor() {
+  }
+
+  ngAfterViewInit(){
+
+    //protection against percentage width and height
+    //e.g. width: 100% and height: 100% dotn have to be equal
+    //with true circle size will be the smallest from width nad height
+    if(this.isEqualSize){
+
+      const widthPx: number = this.circle.nativeElement.clientWidth;
+      const heightPx: number = this.circle.nativeElement.clientHeight;
+
+      if(widthPx != heightPx) this.size = +Math.min(widthPx,heightPx);
+
+      this.circle.nativeElement.style.width = this.size+'px';
+      this.circle.nativeElement.style.height = this.size+'px';
+    }
+
   }
 
 }
