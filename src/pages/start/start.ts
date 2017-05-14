86715309d1d40aa5d050e4babf41a097d0c1251a
pumanitro <pumanitro@gmail.com>
2017-05-14 19:08:00 +0200

    Added new column (7th) and turn counter.

--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -1,4 +1,4 @@
-import {Component, QueryList, ViewChildren} from '@angular/core';
+import {Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { ColOperations } from './col-operations.directive';
@@ -16,13 +16,16 @@ import { ColOperations } from './col-operations.directive';
 export class Start {
 
   @ViewChildren(ColOperations) circles: QueryList<ColOperations>;
+  @ViewChild('turnEl') turnEl: ElementRef;
 
   private player:number = 1;
   private rowsNumber: number = 6;
+  public colsNumber:number = 7;
   private player1Color:string = "yellow";
   private player2Color:string = "green";
+  private turnCounter:number = 1;
 
-  private colLoad:number[] = new Array(this.rowsNumber).fill(0);
+  private colLoad:number[] = new Array(this.colsNumber).fill(0);
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -34,6 +37,7 @@ export class Start {
     return this.player == 1 ? this.player1Color : this.player2Color;
   }
 
+  //for ngFor in view
   createRange(number: number){
     var items: number[] = [];
     for(var i = 0; i < number; i++){
@@ -55,6 +59,9 @@ export class Start {
     cir.elRef.nativeElement.children[0].children[0].style.backgroundColor =  this.playerColor();
 
     this.player = (this.player == 1 ? 2 : 1);
+    this.turnCounter++;
+
+    this.turnEl.nativeElement.textContent = `TURN ${this.turnCounter}`;
 
     //console.log(column);
   }
